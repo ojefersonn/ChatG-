6602210fc3ed4bcfd507cc1d62f882fcbf90f464
Jeferson Oliveira <ojeferson@outlook.com>
2025-04-16 12:02:06 -0300

    Alteração do app.py para rodagem no Streamlit

--- app.py
+++ app.py
@@ -1,8 +1,53 @@
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.vectorstores import Chroma
+
+dados = PyPDFLoader("Dados/D11246.pdf").load()
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=800, chunk_overlap=100)
+textos = text_splitter.split_documents(dados)
+embedding_engine = HuggingFaceEmbeddings(model_name="sentence-transformers/all-mpnet-base-v2")
+vector_db = Chroma.from_documents(textos, embedding_engine)
+
+from langchain import hub
+from langchain_openai import ChatOpenAI
+from langchain.prompts import PromptTemplate
+from langchain_core.runnables import RunnablePassthrough
+from langchain_core.output_parsers import StrOutputParser
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
+n_documentos = 1
+
+def format_docs(documentos):
+    return "\n\n".join(documento.page_content for documento in documentos)
+
+llm = ChatOpenAI(openai_api_key = OPENAI_API_KEY, model = "gpt-4o-mini")
+prompt = hub.pull("rlm/rag-prompt")
+
+rag = (
+    {
+        "question": RunnablePassthrough(),
+        "context": vector_db.as_retriever(k = n_documentos) 
+                    | format_docs
+    }
+    | prompt
+    | llm
+    | StrOutputParser()
+)
+
+prompt = "Quantos artigos tem o decreto 11246?"
+rag.invoke(prompt)
+
+#Streamlit
+
 import streamlit as st
 from langchain.prompts import PromptTemplate
 
 st.set_page_config(page_title="Chat RAG", layout="centered")
-st.title("Chat RAG")
+st.title("Chat Gê")
 
 # Define um prompt fixo
 prompt = PromptTemplate.from_template(
@@ -27,6 +72,6 @@ if st.button("Enviar") or (pergunta and pergunta != st.session_state.get("ultima
         resposta = prompt.format(context="exemplo de contexto", question=pergunta)
         st.session_state.mensagens.append({"pergunta": pergunta, "resposta": resposta})
         st.session_state.ultima_pergunta = pergunta
-        st.experimental_rerun()
+        st._rerun()
     except Exception as e:
         st.error(f"Erro ao formatar o prompt: {e}")
